5a77a78517accd45554ba5bf1654603eeb3c7f28
anotherparsa <anotherparsa@gmail.com>
2025-05-20 20:05:25 +0330

    initialized array

--- DataStructure/Array/main.cpp
+++ DataStructure/Array/main.cpp
@@ -2,11 +2,25 @@
 
 using namespace std;
 
+void show_array(int arr[], int size);
+
 int main(){
-    int arr[5];
-    int sizearray = (sizeof(arr) / sizeof(int));
-    for (int i = 0 ; i < sizearray ; i++){
-        cout << arr[i]<< " ";
+    //un-initialized array
+    int arr1[5];
+    int sizearray = (sizeof(arr1) / sizeof(int));
+    show_array(arr1, sizearray);
+    //initialized array
+    int arr2[] = {1, 2, 3, 4, 5};
+    sizearray = (sizeof(arr1) / sizeof(int));
+    show_array(arr2, sizearray);
+
+
+}
+
+void show_array(int arr[], int size){
+    cout << "Array elements: " << endl;
+    for (int i = 0 ; i < size ; i++){
+        cout << arr[i] << " ";
     }
     cout << endl;
 }
