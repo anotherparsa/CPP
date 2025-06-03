efcafabd27b2a669395470f56b90bd69370a90ab
anotherparsa <anotherparsa@gmail.com>
2025-06-03 11:31:33 +0330

    Arrays

--- DataStructure/Array/main.cpp
+++ DataStructure/Array/main.cpp
@@ -3,9 +3,10 @@
 using namespace std;
 
 void show_array(int arr[], int size);
+void insert_element(int arr[], int &number, int position, int value);
 
 int main(){
-    //un-initialized array
+    /*//un-initialized array
     int arr1[5];
     int sizearray = (sizeof(arr1) / sizeof(int));
     show_array(arr1, sizearray);
@@ -18,7 +19,21 @@ int main(){
     // pre initialized array
     int arr2[] = {1, 2, 3, 4, 5};
     sizearray = (sizeof(arr1) / sizeof(int));
-    show_array(arr2, sizearray);
+    show_array(arr2, sizearray);*/
+    const int max_size = 5;
+    int arr[max_size] = {1, 3};
+    int current_number_of_element = 2;
+    int size = (sizeof(arr) / sizeof(int));
+    cout << "Before inserting an element :" << endl;
+    cout << "Number of element : " << current_number_of_element << endl;
+    show_array(arr, size);
+    insert_element(arr, current_number_of_element, 1, 2);
+    cout << "After Inserting an elment: " << endl;
+    cout << "Number of element : " << current_number_of_element << endl;
+    show_array(arr, size);
+
+
+
 
 
 }
@@ -29,4 +44,13 @@ void show_array(int arr[], int size){
         cout << arr[i] << " ";
     }
     cout << endl;
+}
+
+void insert_element(int arr[], int &number, int position, int value){
+    for (int i = number - 1; i >= position ; i--){
+        arr[i+1] = arr[i];
+    }
+    arr[position] = value;
+    ++number;
+
 }
